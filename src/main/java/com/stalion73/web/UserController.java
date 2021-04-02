481c37e2be18bae8fdbd92b8e46b0d540086d636
augmb <augustomartinez2a@gmail.com>
2021-04-02 15:42:54 +0200

    jofd

--- src/main/java/com/stalion73/web/UserController.java
+++ src/main/java/com/stalion73/web/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController; 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -46,7 +47,8 @@ public class UserController {
 			} else {
 				user=null;
 			}
-            return user;
+            // return ResponseEntity user;
+			return user;
     }
 
     private String getJWTToken(User user) {
