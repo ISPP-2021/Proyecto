981d69767ce1659bc44cd6aaa2ed6e73ca0d4c18
antonio <antferdom@alum.us.es>
2021-04-05 21:37:13 +0200

    MODIFIED added business_id to supplier

--- src/main/java/com/stalion73/configuration/SecurityConfiguration.java
+++ src/main/java/com/stalion73/configuration/SecurityConfiguration.java
@@ -39,7 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/users/new").permitAll()
 			.antMatchers("/admin/**").hasAnyAuthority("admin")
 			.antMatchers(HttpMethod.POST, "/users/login").permitAll()
-			.anyRequest().authenticated()
 			.and()
 				.formLogin()
 				/*.loginPage("/login")*/
@@ -53,7 +52,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			// se sirve desde esta misma página.
 			http.csrf().ignoringAntMatchers("/h2-console/**","/**");
 			http.headers().frameOptions().sameOrigin();
-			http.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 				
 	}
 
