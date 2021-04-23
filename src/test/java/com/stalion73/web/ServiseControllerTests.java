a1183c1e0794f649e6353777c1966829de60c55e
antonio <antferdom@alum.us.es>
2021-04-23 12:30:47 +0200

    FIRST-STEP testing with rest assured

---
+++ src/test/java/com/stalion73/web/ServiseControllerTests.java
@@ -0,0 +1,96 @@
+package com.stalion73.web;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.stalion73.model.Servise;
+import com.stalion73.model.User;
+import com.stalion73.security.JWTAuthorizationFilter;
+import com.stalion73.model.Authorities;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+
+
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+
+import org.assertj.core.api.SoftAssertions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@TestPropertySource("file:src/main/resources/application.properties")
+public class ServiseControllerTests {
+    
+    private Integer id;
+    private Servise servise;
+    private RequestSpecification specification;
+  
+    @Value("${server.port}")
+    private int portNumber;
+  
+    @Test
+    public void one() {
+        User user = new User();
+        user.setUsername("josito");
+        user.setPassword("1234");
+        User usr =  given()
+        .basePath("/users/login")
+        .port(8080)
+        .contentType("application/json")
+        .body(user)
+        .when()
+        .post()
+        .then()
+        .statusCode(200)
+        .extract().body().as(User.class);
+        
+       given()
+        .basePath("/servises")
+        .port(8080)
+        .contentType("application/json")
+        .header("Authorization", usr.getToken())
+        .when()
+        .get()
+        .then()
+        .statusCode(200);
+
+       //get("/servises?id=1").then().statusCode(200);
+
+       //get("/servises?id=1").then().statusCode(200).assertThat()
+        //.body("id", equalTo(1)); 
+    }
+
+    private String getJWTToken(User user) {
+        String secretKey = "mySecretKey";
+        List<Authorities> authorities = new ArrayList<>(user.getAuthorities());
+        String token = Jwts.builder().setId("softtekJWT").setSubject(user.getUsername())
+                .claim("authorities", authorities.stream().map(Authorities::getAuthority).collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + 6000000))
+                .signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
+    }
+  
+}
