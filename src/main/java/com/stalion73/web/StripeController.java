15215e1c5f2f5c77e5f8778f2131ebfe36181e83
augmb <augustomartinez2a@gmail.com>
2021-04-30 20:20:23 +0200

    Stripe fix

--- src/main/java/com/stalion73/web/StripeController.java
+++ src/main/java/com/stalion73/web/StripeController.java
@@ -92,8 +92,7 @@ public class StripeController {
     @PostMapping("/subscription")
     public ResponseEntity<String> webhook(@RequestHeader("Stripe-Signature") String sigHeader,@RequestBody String body){
         String payload = body;
-        String endpointSecret = "whsec_dxRQdnYqUrgppdF8qUQE4dga3u2bWxSJ";
-        System.out.println(sigHeader);
+        String endpointSecret = "whsec_nHPLf6di6qP8zY13CwkLu85DqSDGK6B2";
 
         Event event = null;
 
